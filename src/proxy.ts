010cc3eee2dd1a335b1d0af2ceff4f30cad0b33c
ferbautistamollo <ferbautistamollo@gmail.com>
2026-06-30 17:16:04 -0400

    ajuste de proxy para cerrar sesion en 4 horas

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,20 +1,25 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
 export const proxy = async () => {
-  // QUITAR COMENTARIOS CUANDO SE CREE UN NUEVO PROYECTO
-  // const cookieStore = await cookies();
-  // const cookie = cookieStore.get("msp");
-  // const token = cookie?.value;
-  // const host = process.env.NEXT_PUBLIC_FRONTEND_HOST || "";
-  // const port = process.env.NEXT_PUBLIC_LOGIN_FRONTEND_PORT || "3001";
-  // const url = "http://" + host + ":" + port + "/login";
-  // try {
-  //   if (!token) {
-  //     return NextResponse.redirect(url);
-  //   }
-  //   return NextResponse.next();
-  // } catch (e) {
-  //   console.error("Error verificando token en middleware", e);
-  //   return NextResponse.redirect(url);
-  // }
+  const cookieStore = await cookies();
+  const cookie = cookieStore.get("msp");
+  const token = cookie?.value;
+  const host = process.env.NEXT_PUBLIC_FRONTEND_HOST || "";
+  const port = process.env.NEXT_PUBLIC_LOGIN_FRONTEND_PORT || "3001";
+  const url = "http://" + host + ":" + port + "/login";
+
+  try {
+    if (!token) {
+      return NextResponse.redirect(url);
+    }
+
+    return NextResponse.next();
+  } catch (e) {
+    console.error("Error verificando token en middleware", e);
+
+    return NextResponse.redirect(url);
+  }
 };
 
 export const config = {
