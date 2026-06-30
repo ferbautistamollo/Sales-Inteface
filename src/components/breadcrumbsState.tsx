3477d7ed53d7c524eab7fee0d234033a36e4fbbb
ferbautistamollo <ferbautistamollo@gmail.com>
2026-06-30 18:08:48 -0400

    componente en desarrollo

--- src/components/breadcrumbsState.tsx
+++ src/components/breadcrumbsState.tsx
@@ -1,30 +1,30 @@
 "use client";
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 import { Breadcrumbs } from "@heroui/react";
 
 import { subtitle } from "./primitives";
 
 export const BreadcrumbsState = () => {
-  const pathname = usePathname();
-  const segments = pathname.split("/").filter(Boolean);
+  // const pathname = usePathname();
+  // const segments = pathname.split("/").filter(Boolean);
 
-  const getLabelFromSegment = (segment: string): string => {
-    if (/^template$/.test(segment)) return "Ejemplo";
-    if (/^[0-9a-fA-F-]{36}$/.test(segment)) return "Persona";
+  // const getLabelFromSegment = (segment: string): string => {
+  //   if (/^template$/.test(segment)) return "Ejemplo";
+  //   if (/^[0-9a-fA-F-]{36}$/.test(segment)) return "Persona";
 
-    return segment.replace(/-/g, " ").replace(/\b\w/g, (l) => l.toUpperCase());
-  };
+  //   return segment.replace(/-/g, " ").replace(/\b\w/g, (l) => l.toUpperCase());
+  // };
 
-  const breadcrumbs = segments.map((segment, index) => {
-    const href = "/" + segments.slice(0, index + 1).join("/");
-    const label = getLabelFromSegment(segment);
+  // const breadcrumbs = segments.map((segment, index) => {
+  //   const href = "/" + segments.slice(0, index + 1).join("/");
+  //   const label = getLabelFromSegment(segment);
 
-    return { href, label };
-  });
+  //   return { href, label };
+  // });
 
   return (
     <Breadcrumbs className={subtitle() + ""}>
-      {breadcrumbs.map((crumb, index) => {
+      {/* {breadcrumbs.map((crumb, index) => {
         const isCurrent = index === breadcrumbs.length - 1;
 
         return (
@@ -32,7 +32,7 @@ export const BreadcrumbsState = () => {
             {crumb.label}
           </Breadcrumbs.Item>
         );
-      })}
+      })} */}
     </Breadcrumbs>
   );
 };
