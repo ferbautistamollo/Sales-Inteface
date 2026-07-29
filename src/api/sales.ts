b093235b76fb8409b6207f2ec0414e3198543264
ferbautistamollo <ferbautistamollo@gmail.com>
2026-07-29 11:05:02 -0400

    agregando rutas para llamadas

--- src/api/sales.ts
+++ src/api/sales.ts
@@ -116,7 +116,6 @@ export const getPaymentTypes = async (): Promise<ResponseData> => {
 
 export const postCreateSale = async (body: any): Promise<ResponseData> => {
   try {
-    console.log(body);
     const response = await apiClient.POST(`sales/createSale`, body, true);
     const data = await response.json();
 
@@ -173,9 +172,7 @@ export const getSalesRecords = async (
   personId: string,
 ): Promise<ResponseData> => {
   try {
-    const response = await apiClient.GET(
-      `beneficiaries/persons/records/${personId}`,
-    );
+    const response = await apiClient.GET(`sales/records/${personId}`);
     const data = await response.json();
 
     if (!response.ok) {
@@ -192,8 +189,6 @@ export const getSalesRecords = async (
       data: data.data,
     };
   } catch (e: any) {
-    console.error(e);
-
     return {
       error: true,
       message: "Error al obtener historial de modificaciones",
@@ -202,10 +197,38 @@ export const getSalesRecords = async (
   }
 };
 
-
-export const getPersonSales = async (personId: string): Promise<ResponseData> => {
+export const getPersonSales = async (
+  personId: string,
+): Promise<ResponseData> => {
   try {
-    const response = await apiClient.GET(`sales/${personId}`);
+    const response = await apiClient.GET(`sales/${personId}/sales`);
+    const data = await response.json();
+
+    if (!response.ok) {
+      return {
+        error: true,
+        message: "Ocurrió un error al realizar la petición",
+        data: response.statusText,
+      };
+    }
+
+    return {
+      error: data.error,
+      message: data.message,
+      data: data.data,
+    };
+  } catch (e: any) {
+    return {
+      error: true,
+      message: "Error al obtener datos de los grupos",
+      data: e.message,
+    };
+  }
+};
+
+export const getQrPending = async (personId: string): Promise<ResponseData> => {
+  try {
+    const response = await apiClient.GET(`sales/${personId}/qrPending`);
     const data = await response.json();
 
     if (!response.ok) {
@@ -225,6 +248,61 @@ export const getPersonSales = async (personId: string): Promise<ResponseData> =>
     return {
       error: true,
       message: "Error al obtener datos de los grupos",
+      data: e.message,
+    };
+  }
+};
+
+export const getQrImage = async (qrId: string): Promise<ResponseData> => {
+  try {
+    const response = await apiClient.GET(`sales/${qrId}/qrImage`);
+    const data = await response.json();
+
+    if (!response.ok) {
+      return {
+        error: true,
+        message: "Ocurrió un error",
+        data: response.statusText,
+      };
+    }
+
+    return {
+      error: data.error,
+      message: data.message,
+      data: data.data,
+    };
+  } catch (e: any) {
+    return {
+      error: true,
+      message: "Error al obtener la imagen del qr",
+      data: e.message,
+    };
+  }
+};
+
+export const getVoucherPdf = async (saleId: string): Promise<ResponseData> => {
+  try {
+    const response = await apiClient.GET(
+      `sales/voucherPdf/${saleId}?template=reciboFormal`,
+    );
+
+    if (!response.ok) {
+      return {
+        error: true,
+        message: "Ocurrió un error",
+        data: response.statusText,
+      };
+    }
+
+    return {
+      error: false,
+      message: "Voucher generado con exito",
+      data: response,
+    };
+  } catch (e: any) {
+    return {
+      error: true,
+      message: "Error al obtener pdf de la venta",
       data: e.message,
     };
   }
